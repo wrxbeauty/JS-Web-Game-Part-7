b1fa51fbbf6bdcb31947c002d550fed92ea04f62
Casey Halbohn <wrxbeauty15@hotmail.com>
2023-01-22 10:29:35 -0500

    completed

--- newNonPlayableCharacter.js
+++ newNonPlayableCharacter.js
@@ -23,27 +23,34 @@ function newNonPlayableCharacter(x, y) {
 
     setInterval(moveCharacter, 1)
 
-    function walkEast() {
+    async function walkEast(time) {
         direction = 'east'
         element.src = `./assets/red-character/east.gif`
+        await sleep(time)
+        stop()
     }
 
-    function walkNorth() {
+    async function walkNorth(time) {
         direction = 'north'
         element.src = `./assets/red-character/north.gif`
+        await sleep(time)
+        stop()
     }
 
-    function walkWest() {
+    async function walkWest(time) {
         direction = 'west'
         element.src = `./assets/red-character/west.gif`
+        await sleep(time)
     }
 
-    function walkSouth() {
+    async function walkSouth(time) {
         direction = 'south'
         element.src = `./assets/red-character/south.gif`
+        await sleep(time)
+        stop()
     }
 
-    function stop() {
+    function stop(time) {
         direction = null
         element.src = `./assets/red-character/static.gif`
     }
@@ -57,3 +64,12 @@ function newNonPlayableCharacter(x, y) {
         stop: stop
     }
 }
+
+let time = 5000
+
+function sleep(time){
+    return new Promise(resolve => {
+        setTimeout(resolve, time)
+    })  
+}
+
